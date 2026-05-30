5475786571a03c0f083990faa65e942f12d355ff
Robedson SAINTPHARD <pierreobed67@gmail.com>
2026-05-30 18:34:56 -0300

    feat: implementada interface ExercicioRepository com persistencia JPA

---
+++ src/main/java/com/appfitness/repository/ExercicioRepository.java
@@ -0,0 +1,12 @@
+package com.appfitness.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.appfitness.model.entity.Exercicio;
+
+/**
+ * Interface de acesso ao banco de dados para a entidade Exercicio.
+ * Esta interface deve estender(@extends) JpaRepository para herdar os métodos de CRUD básicos.
+ */
+public interface ExercicioRepository extends JpaRepository<Exercicio, Long> {
+
+}
